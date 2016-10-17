f8757871ddc53deff190c07f315db886be33d832
Kreative27 <ty27mek@wp.pl>
2016-10-17 15:53:44 +0200

    pierwszy commit

---
+++ 17-10-2016/Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_10_2016
+{
+    class Car
+    {
+        private double pojemnoscSilnika;
+        private string marka;
+        private static int iloscKol;
+
+        public Car()
+        { }
+        public Car(string marka, double pojemnoscSilnika)
+        {
+            this.marka = marka;
+            this.pojemnoscSilnika = pojemnoscSilnika;
+        }
+        public static Car Create()
+        {
+            return new Car();
+        }
+
+        public static Car kola()
+        {
+            iloscKol = 4;
+            return new Car();
+        }
+
+        ~Car()
+        {
+            Console.WriteLine("Zwalniam pamięć");
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ 17-10-2016/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_10_2016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car samochod = new Car("Audi", 4.2);
+            Car s2 = Car.kola();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
